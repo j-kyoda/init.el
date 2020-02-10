2abaac2c07b38f525ad2f30e54b5a2484d07224f
j-kyoda <j.kyoda@gmail.com>
2020-02-10 22:03:13 +0900

    Modify python-mode setting

--- init.el
+++ init.el
@@ -198,7 +198,7 @@
 (when (featurep 'python) (unload-feature 'python t))
 (autoload 'python-mode "python-mode" "Python editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+;(add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (add-hook 'python-mode-hook
   '(lambda()
     (setq tab-width 4)
