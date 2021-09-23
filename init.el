029b3100f6d158ad2fa4b060ab3f58b20b8139d7
j-kyoda <j.kyoda@gmail.com>
2021-09-24 00:06:07 +0900

    Modify javascript indent

--- init.el
+++ init.el
@@ -223,6 +223,12 @@
   (package-refresh-contents) (package-install 'js2-mode))
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook
+          '(lambda()
+             (setq js2-basic-offset 2)
+             (setq js2-strict-missing-semi-warning nil)
+             )
+          )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; フレームサイズ最大化・通常化
